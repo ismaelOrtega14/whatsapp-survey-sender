8533856ba4bac2c05be895226f95923cc733b14b
Ismael Ortega <ismael.ortega.works@gmail.com>
2024-09-21 13:45:00 +0200

    build: :construction_worker: Created build files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='whatsapp_survey_sender',
+   version='1.0',
+   description='Send surveis to chats in whatsapp. Made with python and selenium',
+   author='Ismael Ortega',
+   author_email='ismael.ortega.works@gmail.com',
+   packages=['whatsapp_survey_sender'],  #same as name
+   install_requires=['selenium'], #external packages as dependencies
+)
